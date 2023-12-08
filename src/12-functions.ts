68b36fe6b9799f258e94c1ba9c1e49f9a526ba9b
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-08 11:09:28 -0300

    feat: functions

---
+++ src/12-functions.ts
@@ -0,0 +1,33 @@
+(()=>{
+
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+
+  function createProductToJson(
+    title:string,
+    createdAt:Date,
+    stock:number,
+    size:Sizes,
+  ) {
+    return {
+      title,createdAt,stock,size
+    }
+  }
+
+  const producto1 = createProductToJson('P1', new Date(), 12, 'L');
+  console.log('producto1',producto1)
+
+  const createProductToJsonV2 = (
+    title:string,
+    createdAt:Date,
+    stock:number,
+    size?:Sizes,
+  ) => {
+    return {
+      title,createdAt,stock,size
+    }
+  }
+
+  const producto2 = createProductToJsonV2('P1', new Date(), 12);
+  console.log('producto2',producto2)
+
+})();
